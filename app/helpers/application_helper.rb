81ebf1cc3e804b9a356f53cb42c69b159e035731
Andrew Wright <ayax79@gmail.com>
2009-12-06 00:24:31 -0800

    comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  # Prints out the css file includes for blue print
   def blue_print_framework
     stylesheet_link_tag("screen", :media => :screen) +
             stylesheet_link_tag("print", :media => :print) +
