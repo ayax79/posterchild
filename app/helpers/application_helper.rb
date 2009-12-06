be84307e076fb6567d60d7d56157bbfb3aeebc06
Andrew Wright <ayax79@gmail.com>
2009-12-06 00:21:58 -0800

    layout stuff.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def blue_print_framework
+    stylesheet_link_tag("screen", :media => :screen) +
+            stylesheet_link_tag("print", :media => :print) +
+            "<!--[if lt IE 8]>" + stylesheet_link_tag("ie", :media => :screen) + "<![endif]-->"
+  end
+
 end
